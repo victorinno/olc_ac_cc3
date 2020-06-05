a9b0bd3306436937d82558930dfb11fa1d128d76
victorinno <rotc21@gmail.com>
2020-06-05 12:36:31 +0100

    retirar iventario

--- src/cc3/CC3.java
+++ src/cc3/CC3.java
@@ -83,11 +83,9 @@ public class CC3 {
 			try {
 				a.menuDefesas(lutador);
 			} catch (FileNotFoundException ex) {
-				Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null,
-						ex);
+				Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null, ex);
 			} catch (IOException ex) {
-				Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null,
-						ex);
+				Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null, ex);
 			}
 		} else if (opt == 5) {
 			System.out.println(lutador.displayStatus());
@@ -136,8 +134,7 @@ public class CC3 {
 			}
 		} else if (opt == 3) {
 			for (int i = 0; i < lutador.getPoderes().size(); i++) {
-				System.out.println((i + 1) + " - "
-						+ lutador.getPoderes().get(i).toString());
+				System.out.println((i + 1) + " - " + lutador.getPoderes().get(i).toString());
 			}
 			sc = new Scanner(System.in);
 			int p = sc.nextInt();
@@ -145,8 +142,7 @@ public class CC3 {
 		}
 	}
 
-	public void menuDefesas(Lutador lutador) throws FileNotFoundException,
-			IOException {
+	public void menuDefesas(Lutador lutador) throws FileNotFoundException, IOException {
 		System.out.println("Escolha a baixo: ");
 		System.out.println("1 - Criar Defesa: ");
 		System.out.println("2 - Listar Defesas: ");
@@ -167,8 +163,7 @@ public class CC3 {
 			}
 		} else if (opt == 3) {
 			for (int i = 0; i < lutador.getDefesas().size(); i++) {
-				System.out.println((i + 1) + " - "
-						+ lutador.getDefesas().get(i).toString());
+				System.out.println((i + 1) + " - " + lutador.getDefesas().get(i).toString());
 			}
 			System.out.println("Escolha sua defesa: ");
 			sc = new Scanner(System.in);
@@ -223,7 +218,7 @@ public class CC3 {
 			Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (IOException ex) {
 			Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null, ex);
-		} 
+		}
 	}
 
 	public void criarDefesa(Lutador lutador) {
@@ -273,8 +268,7 @@ public class CC3 {
 			try {
 				writer.close();
 			} catch (IOException ex) {
-				Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null,
-						ex);
+				Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null, ex);
 			}
 		}
 	}
@@ -285,28 +279,28 @@ public class CC3 {
 			Random r = new Random();
 			Integer cf = a.getCl().getForca();
 			Integer rolagem = r.nextInt(101);
-			
+
 			if (rolagem <= cf) {
 				a.setForca(a.getForca() + 1);
 				System.out.println(a.getNome() + " aumentou sua forca");
 			}
 			Integer cp = a.getCl().getPericia();
 			rolagem = r.nextInt(101);
-			
+
 			if (rolagem <= cp) {
 				a.setPericia(a.getPericia() + 1);
 				System.out.println(a.getNome() + " aumentou sua pericia");
 			}
 			Integer ca = a.getCl().getAgilidade();
 			rolagem = r.nextInt(101);
-			
+
 			if (rolagem <= ca) {
 				a.setAgilidade(a.getAgilidade() + 1);
 				System.out.println(a.getNome() + " aumentou sua agilidade");
 			}
 			Integer cr = a.getCl().getResistencia();
 			rolagem = r.nextInt(101);
-			
+
 			if (rolagem <= cr) {
 				a.setResistencia(a.getResistencia() + 1);
 				System.out.println(a.getNome() + " aumentou sua resistencia");
@@ -319,8 +313,7 @@ public class CC3 {
 		}
 	}
 
-	public void persistirLutador(Lutador a) throws IOException,
-			FileNotFoundException {
+	public void persistirLutador(Lutador a) throws IOException, FileNotFoundException {
 		XStream xstream = new XStream(new DomDriver());
 		File perArq = new File("C:/co1/lutadores/" + a.getNome() + ".xml");
 		Writer writer = new PrintWriter(perArq);
@@ -333,7 +326,7 @@ public class CC3 {
 			if (a.getPowerUp() == null) {
 				a.setPowerUp(0);
 			}
-			a.getIventorio().getArmas()[0].ativar(a);
+//			a.getIventorio().getArmas()[0].ativar(a);
 			Random r = new Random();
 			Integer pp = +r.nextInt(3) + 1;
 			System.out.println("PowerUP: " + pp);
@@ -341,8 +334,9 @@ public class CC3 {
 			evolucao(a);
 			persistirLutador(a);
 			System.out.println((a.getPericia()
-					+ a.getIventorio().getArmas()[0].getDano()) + "/" + (a.getForca() 
-					+ a.getIventorio().getArmas()[0].getAtaque()) + "/" + a.getNome() + " ataca com " + a.getIventorio().getArmas()[0].getNome());
+			/* + a.getIventorio().getArmas()[0].getDano() */) + "/" + (a.getForca()
+			/* + a.getIventorio().getArmas()[0].getAtaque() */) + "/" + a.getNome() + " ataca"/*om "*/
+					/*+ a.getIventorio().getArmas()[0].getNome()*/);
 		} catch (FileNotFoundException ex) {
 			Logger.getLogger(CC3.class.getName()).log(Level.SEVERE, null, ex);
 
@@ -355,20 +349,18 @@ public class CC3 {
 	public void usarPoder(Integer poder, Lutador a) {
 		Poder p = a.getPoderes().get(poder - 1);
 		if (a.getPowerUp() >= p.getNivel()) {
-			
+
 //			a.getIventorio().getAcessorios()[0].ativar(a);
 //			int apup = a.getIventorio().getAcessorios()[0].pup();
-			a.setPowerUp(p.getNivel() == 1 ? a.getPowerUp() - 0 : a
-					.getPowerUp() - p.getNivel() /*+ apup*/);
-			System.out.println("Powerup - " + p.getNivel() + " = "
-					+ a.getPowerUp() /*+ apup*/);
+			a.setPowerUp(p.getNivel() == 1 ? a.getPowerUp() - 0 : a.getPowerUp() - p.getNivel() /* + apup */);
+			System.out.println("Powerup - " + p.getNivel() + " = " + a.getPowerUp() /* + apup */);
 			Integer pericia = a.getPericia() + (p.getPericia() * p.getNivel());
 			Integer forca = a.getForca() + (p.getForca() * p.getNivel());
 			System.out.println(pericia + "/" + forca + "/" + p.getNome());
 			evolucaoPoder(poder, a);
 			Random r = new Random();
 			Integer pp = +r.nextInt(3) + 1;
-			
+
 			a.setPowerUp(a.getPowerUp() + pp);
 			evolucao(a);
 		} else {
@@ -377,27 +369,22 @@ public class CC3 {
 		}
 	}
 
-	public void usarDefesa(Integer defesa, Lutador a, String ataque)
-			throws FileNotFoundException, IOException {
+	public void usarDefesa(Integer defesa, Lutador a, String ataque) throws FileNotFoundException, IOException {
 		Defesa p = a.getDefesas().get(defesa - 1);
 		if (a.getDefenseUp() >= p.getNivel()) {
-			
-			a.getIventorio().getAcessorios()[0].ativar(a);
-			a.setDefenseUp(p.getNivel() == 1 ? a.getDefenseUp() : a
-					.getDefenseUp() - p.getNivel());
 
-			System.out.println("DefenseUp - " + p.getNivel() + " = "
-					+ a.getDefenseUp());
-			Integer agilidade = a.getAgilidade()
-					+ (p.getAgilidade() * p.getNivel());
-			Integer resistencia = a.getResistencia()
-					+ (p.getResistencia() * p.getNivel());
+//			a.getIventorio().getAcessorios()[0].ativar(a);
+			a.setDefenseUp(p.getNivel() == 1 ? a.getDefenseUp() : a.getDefenseUp() - p.getNivel());
+
+			System.out.println("DefenseUp - " + p.getNivel() + " = " + a.getDefenseUp());
+			Integer agilidade = a.getAgilidade() + (p.getAgilidade() * p.getNivel());
+			Integer resistencia = a.getResistencia() + (p.getResistencia() * p.getNivel());
 
 			String[] atks = ataque.split("/");
 			System.out.println(p.getNome() + " VS " + atks[2]);
 			Integer per = new Integer(atks[0]);
 			Integer chance = (int) ((per / agilidade) * 50 * (1.2 - (Math.random() * 0.40)));
-			
+
 			Random r = new Random();
 			Integer rolagem = r.nextInt(101);
 			if (chance > 95) {
@@ -406,8 +393,8 @@ public class CC3 {
 				chance = 5;
 			}
 
-			int adup = a.getIventorio().getAcessorios()[0].dup();
-			Integer dp = r.nextInt(3) + 1 + adup;
+//			int adup = a.getIventorio().getAcessorios()[0].dup();
+			Integer dp = r.nextInt(3) + 1 /*+ adup*/;
 			System.out.println("DefenseUP: +" + dp);
 			a.setDefenseUp(a.getDefenseUp() + dp);
 			evolucao(a);
@@ -418,9 +405,7 @@ public class CC3 {
 				if (cri > 0) {
 					System.out.println("Critico! " + cri);
 				}
-				Integer dano = (int) (new Integer(atks[1]) / resistencia + ((Math
-						.random() * 4) + 1))
-						+ cri;
+				Integer dano = (int) (new Integer(atks[1]) / resistencia + ((Math.random() * 4) + 1)) + cri;
 				dano = dano < 1 ? 1 : dano;
 				System.out.println("Dano: " + dano);
 				a.setHp(a.getHp() - dano);
@@ -443,17 +428,15 @@ public class CC3 {
 		Random r = new Random();
 		int cf = r.nextInt(101);
 		int cp = r.nextInt(101);
-	
+
 		if (cf <= p.getChanceForca()) {
 			p.setForca(p.getForca() + 1);
-			System.out
-					.println("Poder: " + p.getNome() + " aumentou em forca!");
+			System.out.println("Poder: " + p.getNome() + " aumentou em forca!");
 		}
 
 		if (cp <= p.getChancePericia()) {
 			p.setPericia(p.getPericia() + 1);
-			System.out.println("Poder: " + p.getNome()
-					+ " aumentou em pericia!");
+			System.out.println("Poder: " + p.getNome() + " aumentou em pericia!");
 		}
 
 	}
@@ -466,14 +449,12 @@ public class CC3 {
 
 		if (cf <= p.getChanceAgilidade()) {
 			p.setAgilidade(p.getAgilidade() + 1);
-			System.out
-					.println("Poder: " + p.getNome() + " aumentou em agilidade!");
+			System.out.println("Poder: " + p.getNome() + " aumentou em agilidade!");
 		}
 
 		if (cp <= p.getChanceResistencia()) {
 			p.setResistencia(p.getResistencia() + 1);
-			System.out.println("Poder: " + p.getNome()
-					+ " aumentou em resistencia!");
+			System.out.println("Poder: " + p.getNome() + " aumentou em resistencia!");
 		}
 
 	}
@@ -491,11 +472,11 @@ public class CC3 {
 
 	public void calcularDefesa(String ataque, Lutador a) {
 		String[] atkS = ataque.split("/");
-		a.getIventorio().getArmaduras()[0].ativar(a);
+//		a.getIventorio().getArmaduras()[0].ativar(a);
 		System.out.println("O atacante usou: " + atkS[2]);
-		System.out.println("Você se denfede com: " + a.getIventorio().getArmaduras()[0].getNome());
+		System.out.println("Você se denfede"/* com: " + a.getIventorio().getArmaduras()[0].getNome()*/);
 		Integer per = new Integer(atkS[0]);
-		Integer chance = (int) (per / (a.getAgilidade() + a.getIventorio().getArmaduras()[0].getDefesa())) * 50;
+		Integer chance = (int) (per / (a.getAgilidade() /*+ a.getIventorio().getArmaduras()[0].getDefesa()*/)) * 50;
 		Random r = new Random();
 		Integer rolagem = (int) (r.nextInt(101) * (1.2 - (Math.random() * 0.40)));
 		Integer critico = chance - 95;
@@ -505,8 +486,8 @@ public class CC3 {
 			chance = 5;
 		}
 
-		//System.out.println("Chance: " + chance);
-		//System.out.println("Rolagem: " + rolagem);
+		// System.out.println("Chance: " + chance);
+		// System.out.println("Rolagem: " + rolagem);
 		Integer dp = r.nextInt(3) + 1;
 		System.out.println("DefenseUP: +" + dp);
 		a.setDefenseUp(a.getDefenseUp() + dp);
@@ -571,11 +552,10 @@ public class CC3 {
 		Writer writer = null;
 		try {
 			Integer forc = new Integer(forca);
-			Integer dano = (forc / a.getResistencia()) < 1 ? 1 : (forc / a
-					.getResistencia());
-			
-			Integer danoTotal = (int) (dano + multi - a.getIventorio().getArmaduras()[0].getReducao() + ((Math
-					.random() * 4) + 1));
+			Integer dano = (forc / a.getResistencia()) < 1 ? 1 : (forc / a.getResistencia());
+
+			Integer danoTotal = (int) (dano + multi /*- a.getIventorio().getArmaduras()[0].getReducao()*/
+					+ ((Math.random() * 4) + 1));
 			System.out.println("Dano: " + danoTotal);
 			a.setHp(a.getHp() - danoTotal);
 			System.out.println("Vida: " + a.getHp() + "%");
@@ -596,8 +576,7 @@ public class CC3 {
 		ClasseLutadora guerreiro = new ClasseLutadora("Guerreiro", 6, 2, 2, 6);
 		ClasseLutadora duelista = new ClasseLutadora("Duelista", 6, 6, 2, 2);
 		ClasseLutadora ninja = new ClasseLutadora("Ninja", 6, 2, 6, 2);
-		ClasseLutadora artistaMarcial = new ClasseLutadora("Artista Marcial",
-				2, 2, 6, 6);
+		ClasseLutadora artistaMarcial = new ClasseLutadora("Artista Marcial", 2, 2, 6, 6);
 		ClasseLutadora estudante = new ClasseLutadora("Estudante", 4, 4, 4, 4);
 		ClasseLutadora mago = new ClasseLutadora("Mago", 10, 2, 2, 2);
 		ClasseLutadora semideus = new ClasseLutadora("Semi Deus", 2, 2, 2, 10);
@@ -680,8 +659,7 @@ public class CC3 {
 			File arquivo = new File("/classes/" + op + ".xml");
 			FileReader fr = new FileReader(arquivo);
 			ClasseLutadora cl = (ClasseLutadora) xstream.fromXML(fr);
-			Lutador l = new Lutador(nome, cl, new Integer("1"),
-					new Integer("1"), new Integer("1"), new Integer("1"));
+			Lutador l = new Lutador(nome, cl, new Integer("1"), new Integer("1"), new Integer("1"), new Integer("1"));
 
 			File perArq = new File("C:/co1/lutadores/" + nome + ".xml");
 			// FileOutputStream fos = new FileOutputStream(perArq);
